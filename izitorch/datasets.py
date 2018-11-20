78e87f6f6378098d98fb031dd08ddcfa39d58eec
vsfg <vivien.sainte-fare-garnot@polytechnique.edu>
2018-11-20 22:24:06 +0100

    [DEV] wip

--- izitorch/datasets.py
+++ izitorch/datasets.py
@@ -16,8 +16,7 @@ class Sequential_Dataset_from_h5folder(data.Dataset):
         self.folder_path = folder
         self.labels = labels
         self.extra_feature = extra_feature
-        self.im_transforms = im_transforms if type(im_transforms) == list else [im_transforms for _ in
-                                                                                range(10)]  # TODO do this better
+        self.im_transforms = im_transforms
         self.extra_transform = extra_transform
         self.dataset_list = np.sort([f for f in os.listdir(folder) if str(f).endswith('.h5')])
         with h5py.File(os.path.join(folder, self.dataset_list[0]), 'r') as h5:
@@ -31,9 +30,9 @@ class Sequential_Dataset_from_h5folder(data.Dataset):
         with h5py.File(os.path.join(self.folder_path, self.dataset_list[dataset_number]), 'r') as h5:
 
             image_series = (torch.from_numpy(h5['images'][local_index, :, :, :, :]).float())
-            if self.im_transform is not None:
+            if self.im_transforms is not None:
                 for i in range(image_series.size()[0]):
-                    image_series[i, :, :, :] = (self.im_transform[i])(image_series[i, :, :, :])
+                    image_series[i, :, :, :] = (self.im_transforms[i])(image_series[i, :, :, :])
 
             if self.extra_feature is not None:
                 extra = torch.from_numpy(np.array(h5[self.extra_feature][local_index], dtype=int)).float()
@@ -58,11 +57,10 @@ class Sequential_Dataset_from_h5folder(data.Dataset):
         return self.len
 
     def compute_stats(self, out_path='./meanstd.pkl'):
-        dl = data.DataLoader(self, batch_size=1000, shuffle=False, num_workers=6)
+        dl = data.DataLoader(self, batch_size=100, shuffle=False, num_workers=6)
 
         m = []
         s = []
-
 
         xm = []
         xs = []
@@ -70,14 +68,17 @@ class Sequential_Dataset_from_h5folder(data.Dataset):
         for x, y in tqdm(dl):
 
             if self.extra_feature is not None:
-                x,extra = x
-                xm.append(np.mean(extra.numpy(),axis=0))
+                x, extra = x
+                xm.append(np.mean(extra.numpy(), axis=0))
                 xs.append(np.std(extra.numpy(), axis=0))
 
-            means.append(np.mean(x.numpy(), axis=(0, 3, 4)))
-            stds.append(np.std(x.numpy(), axis=(0, 3, 4)))
-
-        stats = {'images':(m,s),'extra':(xm,xs)}
+            m.append(np.mean(x.numpy(), axis=(0, 3, 4)))
+            s.append(np.std(x.numpy(), axis=(0, 3, 4)))
+        m = np.mean(m,axis=0)
+        s = np.mean(s,axis=0)
+        xm = np.mean(xm, axis=0)
+        xs = np.mean(xs, axis=0)
+        stats = {'images': (m, s), 'extra': (xm, xs)}
         pkl.dump(stats, open(out_path, 'wb'))
 
 
