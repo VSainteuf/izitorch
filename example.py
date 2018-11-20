8b27c7aa16732910ea2386e9c3459476ebec7409
vsfg <vivien.sainte-fare-garnot@polytechnique.edu>
2018-11-20 18:24:45 +0100

    [DEV] wip

--- example.py
+++ example.py
@@ -4,43 +4,36 @@ from torch.utils import data
 
 from trainRack import Rack
 
+model = nn.GRU(10, 20, 1)
 
-
-model = nn.GRU(10,20,1)
 
 class RandDataset(data.Dataset):
-    def __init__(self,nsamp=10000,seqlen=5,nfeat=10,nclass=2):
+    def __init__(self, nsamp=10000, seqlen=5, nfeat=10, nclass=2):
         super(RandDataset, self).__init__()
         self.nsamp = nsamp
         self.nfeat = nfeat
-        self.data = torch.randn(nsamp,seqlen,nfeat+nclass)
+        self.data = torch.randn(nsamp, seqlen, nfeat + nclass)
+
     def __getitem__(self, item):
-        return self.data[item,:,:nfeat],self.data[item,:,nfeat:]
+        return self.data[item, :, :self.nfeat], self.data[item, :, self.nfeat:]
 
     def __len__(self):
         return self.nsamp
 
 
-arg_dict = {'extra_feature':{'default':2,'type':int}}
+arg_dict = {'extra_feature': {'default': 2, 'type': int}}
 optimizer_class = torch.optim.Adam
 criterion = nn.CrossEntropyLoss()
 
-
-
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     rack = Rack()
 
     rack.add_arguments(arg_dict)
     rack.parse_args()
 
-
     rack.set_model(model)
     rack.set_optimizer(optimizer_class)
     rack.set_criterion(criterion)
-    rack.dataset = RandDataset()
+    rack.set_dataset(RandDataset())
 
     rack.launch()
-
--- trainRack.py
+++ trainRack.py
@@ -1,47 +1,107 @@
+import torch #TODO minimal import
+from torch.utils import data
 
+import argparse
+
+import json
+import os
 
 
 class Rack:
 
     def __init__(self):
-        self.dataset = None
         self.set_basic_menu()
         self.set_device()
 
-        pass
+    def to_dict(self):
+        d = vars(self.args).copy()
+        d['model'] = str(self.model)
+        d['criterion'] = str(self.criterion)
+        d['optimizer'] = str(self.optimizer)
+        d['device'] = str(self.device)
+        return d
 
-    def set_model(self,model):
-        pass
-
+    ####### Methods for the options menu
     def set_basic_menu(self):
-        pass
+        parser = argparse.ArgumentParser()
 
-    def set_device(self):
-        pass
+        parser.add_argument('--dataset', default='/home/vsfg/data')
+        parser.add_argument('--res_dir', default='results', help='folder for saving the trained model')
+        parser.add_argument('--resume', default='')
 
-    def set_optimizer(self,optimizer_class):
-        pass
+        parser.add_argument('--num_classes', default=44, type=int)
 
-    def set_criterion(selfself,criterion):
-        pass
+        parser.add_argument('--batch_size', default=128, type=int, help='Batch size')
+        parser.add_argument('--shuffle', default=True, help='Shuffle dataset')
+        parser.add_argument('--num_workers', default=6, type=int, help='number of workers for data loader')
+        parser.add_argument('--train_ratio', default=.8, type=float, help='ratio for train/test split')
 
-    def add_arguments(self,arg_dict):
-        pass
+        parser.add_argument('--epochs', default=1000, type=int)
+        parser.add_argument('--lr', default=1e-3, type=float, help='Initial learning rate')
+        parser.add_argument('--test_step', default=10, type=int, help='Test model every that many steps')
+
+        self.parser = parser
+
+    def add_arguments(self, arg_dict):
+        for name, setting in arg_dict.items():
+            self.parser.add_argument('--{}'.format(name), default=setting['default'], type=setting['type'])
 
     def parse_args(self):
-        pass
+        self.args = self.parser.parse_args()
+
+    def print_args(self):
+        print(self.args)
+
+    ####### Methods for setting the specific elements of the training rack
+
+    def set_device(self, device_name=None):
+        try:
+            self.device(torch.device(device_name))
+        except TypeError:
+            self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    def set_model(self, model):
+        self.model = model
+
+    def set_optimizer(self, optimizer_class):
+        self.optimizer = optimizer_class(self.model.parameters())
+
+    def set_criterion(self, criterion):
+        self.criterion = criterion
+
+    def set_dataset(self, dataset):
+        self.dataset = dataset
+
+    ####### Methods for execution
 
     def prepare_output(self):
-        pass
+        if not os.path.exists(self.args.res_dir):
+            os.makedirs(self.args.res_dir)
+        else:
+            print('WARNING: Output directory  already exists')
+
+        with open(os.path.join(self.args.res_dir, 'conf.json'), 'w') as fp:
+            json.dump(self.to_dict(), fp, indent=4)
 
     def get_loaders(self):
-        return (None, None)
+
+        print('Splitting dataset')
+        ntrain = int(np.floor(self.args.train_ratio * len(self.dataset)))
+        ntest = len(self.dataset) - ntrain
+
+        dtrain, dtest = data.random_split(self.dataset, [ntrain, ntest])
+        print('Train: {} samples, Test : {} samples'.format(ntrain, ntest))
+
+        train_loader = data.DataLoader(dtrain, batch_size=self.args.batch_size, shuffle=self.args.shuffle,
+                                       num_workers=self.args.num_workers)
+        test_loader = data.DataLoader(dtest, batch_size=self.args.batch_size, shuffle=self.args.shuffle,
+                                      num_workers=self.args.num_workers)
+        return train_loader, test_loader
 
     def launch(self):
-        pass
         self.prepare_output()
 
-        self.train_loader,self.test_loader = self.get_loaders()
+        self.train_loader, self.test_loader = self.get_loaders()
 
         self.train()
 
