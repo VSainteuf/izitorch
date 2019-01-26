54976ece15ae040ad9d17f7156125ca2e31a0f9e
VSainteuf <vivien.sainte-fare-garnot@polytechnique.edu>
2019-01-26 13:21:24 +0100

    [REFACTO] docstring

--- izitorch/classif_results.py
+++ izitorch/classif_results.py
@@ -1,3 +1,8 @@
+"""
+Set of helper classes to parse and explore the checkpoints and trainlogs of the train rack, in case of a classification
+problem.
+"""
+
 import os
 import json
 import pickle as pkl
@@ -7,6 +12,13 @@ import numpy as np
 
 class FoldResult:
     def __init__(self, folder):
+        """
+        Initialise and parse the trainlog, confusion_matrix and configuration of the fold.
+        Also directly computes the final performance achieved.
+
+        Args:
+            folder (str): path to the directory of the fold's outputs
+        """
         self.folder = folder
         self.name = self._parse_name()
         self.trainlog_path = os.path.join(folder, 'trainlog.json')
@@ -52,21 +64,40 @@ class FoldResult:
         return metrics(self.conf_mat)
 
     def get_series(self, columns):
+        """
+        Gets the specified set of metrics from the trainlog.
+        Args:
+            columns (list): names of the columns to be retrieved (e.g. 'accuracy', 'loss', 'val_accuracy')
+
+        Returns:
+            df (pandas.DataFrame) :  DataFrame with the values of the selected metrics for all the epochs
+        """
         return self.trainlog[columns]
 
     def get_confmat(self):
+        """Gets the confusion matrix
+        """
         return self.conf_mat
 
     def get_per_class(self):
+        """Gets the per_class performance"""
         return self.per_class
 
     def get_overall(self):
+        """Gets the overall performance"""
         return self.overall
 
 
 class ModelResult:
 
     def __init__(self, folder):
+        """
+          Initialise and parse the trainlog, confusion_matrix and configuration of the model.
+          If the model was trained with K-fold, all the folds are parsed and combined to compute performance.
+
+          Args:
+              folder (str): path to the directory of the model's outputs
+          """
         self.folder = folder
         self.name = self._parse_name()
         self.conf = self._parse_conf()
@@ -133,10 +164,25 @@ class ModelResult:
 class Comparator:
 
     def __init__(self, model_folder_list):
+        """
+        Helper class to compare the results of different models
+
+        Args:
+            model_folder_list (list): list of the paths to the models that should be compared
+        """
         self.folders = model_folder_list
         self.models = [ModelResult(f) for f in model_folder_list]
 
     def overall(self, metrics=['Precision', 'Recall', 'F1-score', 'IoU'], mode='MACRO'):
+        """
+        Assembles the overall performances of the different models in a single table.
+        Args:
+            metrics (list): list of the metrics that should be used for comparison
+            mode (str): which of MICRO or MACRO averages to choose to aggregate the per class metrics
+
+        Returns:
+            df (pandas.DataFrame): DataFrame with the specified metrics for all models under consideration
+        """
         columns = ['{}_{}'.format(mode, met) for met in metrics if met != 'Accuracy']
         if 'Accuracy' in metrics:
             columns.append('Accuracy')
@@ -148,16 +194,35 @@ class Comparator:
             df.loc[m.name] = p
         return df
 
-    def per_class(self,metric='F1-score'):
+    def per_class(self, metric='F1-score'):
+        """
+        Assembles the per class performance of all models for a specific metric, in a single table.
+        Args:
+            metric (str): metrics that should be used for comparison
+
+        Returns:
+            df (pandas.DataFrame): DataFrame with the per class performance for all models under consideration.
+
+        """
         df = pd.DataFrame()
         for m in self.models:
             df[m.name] = pd.DataFrame(m.get_per_class()).transpose()[metric]
         return df
 
 
-
-
 def metrics(mat):
+    """
+    This method computes all the performance metrics from the confusion matrix. In addition to overall accuracy, the
+    precision, recall, f-score and IoU for each class is computed.
+    The class-wise metrics are averaged to provide overall indicators in two ways (MICRO and MACRO average)
+    Args:
+        mat (array): confusion matrix
+
+    Returns:
+        per_class (dict) : per class metrics
+        overall (dict): overall metrics
+
+    """
     TP = 0
     FP = 0
     FN = 0
